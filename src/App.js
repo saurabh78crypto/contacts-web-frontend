59f1747e7b661d04bd15d86cdddb6ae2dc14d483
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-09-22 16:13:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import ContactsPage from './pages/ContactsPage';
 import MessagesPage from './pages/MessagesPage';
 import ContactDetail from './components/ContactDetail'; 
@@ -13,12 +13,16 @@ const App = () => {
         <Navbar.Brand as={Link} to="/contacts">Contacts App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto"> 
+          <Nav className="ms-auto">
             <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
             <Nav.Link as={Link} to="/messages">Messages Sent</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <Container className="mt-4">
+        <h1>Welcome to the Contacts Web App</h1>
+        <p>Manage your contacts and send messages effortlessly.</p>
+      </Container>
       <Routes>
         <Route path="/contacts" element={<ContactsPage />} />
         <Route path="/contact/:phone" element={<ContactDetail />} />
