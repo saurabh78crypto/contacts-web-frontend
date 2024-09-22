8a694b490e0733138a34f6234259d30aef000fd8
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-09-22 16:18:21 +0530

    done

--- src/pages/ContactsPage.js
+++ src/pages/ContactsPage.js
@@ -5,7 +5,6 @@ import { Container } from 'react-bootstrap';
 const ContactsPage = () => {
   return (
     <Container>
-      <h1 className="mt-4 mb-4 text-center">Contacts</h1>
       <ContactList />
     </Container>
   );
